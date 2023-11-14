3beffc1d022a67c081304d4ccf30e2e5464d8843
mvogelgesang <mvogelgesang@salesforce.com>
2023-11-13 21:41:06 -0500

    Added GitHub repo step

--- create.sh
+++ create.sh
@@ -6,10 +6,31 @@ datedAlias=$(date '+%Y%m%d')_
 scratchDef="utilities/..scratchDefs/default.json"
 devHub="pboDevHub"
 folder="pboDevHub"
+github=false
 
 ## Start script
+echo "DevHub (leave blank for default $devHub)"
+read o
+if [ ! -z "$o" ]
+  then
+    devHub=$o
+fi
 
 echo "This script will create a new scratch org off of $devHub. Checking pre-conditions..."
+
+if ! command -v sf
+then
+    echo "Salesforce CLI could not be found. You must install this first. Exiting..."
+    exit 1
+fi
+
+if ! command -v gh
+  then
+    echo "GitHub CLI could not be found, GitHub steps will be skipped"
+    exit 1
+  else
+    github=true
+fi
 
 # check if code is a valid command in the terminal, if not, direct user to help resources
 if ! command -v code
@@ -52,7 +73,7 @@ sf org resume scratch --use-most-recent
 echo "Scratch org creation done"
 echo "Setting target-org and generating project"
 sf project generate -t standard -n $datedAlias -d $folder
-code $folder/$datedAlias
+code $folder/$datedAlias -g $folder/$datedAlias/README.md:2
 cd $folder/$datedAlias
 sf config set target-org=$alias
 echo "Resetting the password"
@@ -66,6 +87,15 @@ module.exports = {
   \"**/*.cls\": (filenames) => \"sf scanner run -f table -s 3 -t \" + filenames.join(\", \") 
 };
 " > lint-staged.config.js
+
+if $github
+then
+  echo "Creating a git repo locally and on GitHub"
+  git init 
+  gh repo create $datedAlias --private  -s .  --disable-wiki --disable-issues
+else
+  echo "Github CLI not setup, skipping Git-related steps"
+fi
 
 echo "Opening the new org"
 sf org open -o $alias
