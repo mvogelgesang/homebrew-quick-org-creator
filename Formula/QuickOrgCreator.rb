2c96ec3cf3e411a139dfc92d9021b53d5fd5c6c1
mvogelgesang <mvogelgesang@salesforce.com>
2024-04-14 21:59:50 -0400

    Updating formula definition for v0.2.0

--- Formula/QuickOrgCreator.rb
+++ Formula/QuickOrgCreator.rb
@@ -4,8 +4,8 @@
 class QuickOrgCreator < Formula
   desc "A script to ease the process of creating Salesforce scratch orgs"
   homepage "https://github.com/mvogelgesang/homebrew-quick-org-creator"
-  url "https://github.com/mvogelgesang/homebrew-quick-org-creator/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3b1d62ac4b8e27bc2858fb7f2b83e8776f54ee3cca360d66244be87be1f10727"
+  url "https://github.com/mvogelgesang/homebrew-quick-org-creator/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "2ca040070a0c0b932cc088cae715c079896404650464f21de9f44ad2a08a9b4b"
   license "MIT"
 
   depends_on "gh"
