b1790366ed0da351060f585c9dee2adc1ec5b11c
mvogelgesang <mvogelgesang@salesforce.com>
2024-07-20 22:29:39 -0400

    Added echo statements to let user know that devhub list is being refreshed and once it is complete

--- src/devHub.sh
+++ src/devHub.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 source ${oc_installedDir}/functions.sh
 
+echo "Refreshing list of authenticated DevHubs..."
 results=($(sf org list --json | jq '.result.devHubs[] | .alias'))
 
 # Convert the array to a space-separated string
 results_string=$(IFS=" "; echo "${results[*]}")
 
 update_or_add_var "oc_devHubArray" "${oc_configFileName}" "${results[@]}"
+echo -e "${oc_COLOR_SUCCESS}Done.${oc_COLOR_NOCOLOR}"
+echo ""
