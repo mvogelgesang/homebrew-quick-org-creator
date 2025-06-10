a4e906cdda0548722d2274c6425cab8d84a94ca3
mvogelgesang <mvogelgesang@salesforce.com>
2025-06-10 10:01:44 -0400

    #20 initial function creation and migration of title to _message function

--- src/run.sh
+++ src/run.sh
@@ -10,32 +10,11 @@ export oc_configFileName="${oc_configFilePath}.config"
 export oc_github=false
 export oc_currentWorkingDirectory=$(pwd)
 
+source ${oc_installedDir}/functions.sh
 
-## Colors
-export oc_COLOR_WHITE='\033[0;37m' #cWhite
-# ='\033[0;34m'  #cBlue
-# ='\033[0;36m' #cCyan
-export oc_COLOR_QUESTION='\033[0;96m' #cLightCyan
-#='\033[0;94m' #cLightBlue
-export oc_COLOR_SUCCESS='\033[0;32m' #cGreen
-export oc_COLOR_WARN='\033[0;33m' #cYellow
-export oc_COLOR_THEME='\033[0;35m' #cMagenta
-export oc_COLOR_ERROR='\033[0;31m' #cRed
-export oc_COLOR_NOTIFICATION='\033[0;44m' #bBlue
-#='\033[0;46m' #bCyan
-#='\033[0;104m' #bLightBlue
-#='\033[0;106m' #bLightCyan
-export oc_COLOR_NOCOLOR='\033[0;0m' #cNoColor
-export oc_arrow="  -> "
 version=$(cat ${oc_installedDir}/VERSION)
 
-echo -e "${oc_COLOR_THEME}
-===================================================
- __   __   __      __   __   ___      ___  __   __  
-/  \ |__) / _\`    /  \` |__) |__   /\   |  /  \ |__) 
-\__/ |  \ \__>    \__, |  \ |___ /~~\  |  \__/ |  \\
-=================================================== 
-${oc_COLOR_NOCOLOR}"                                                    
+_message "theme" "$oc_TITLE"                          
 
 source "${oc_installedDir}/dependencies.sh"
 
