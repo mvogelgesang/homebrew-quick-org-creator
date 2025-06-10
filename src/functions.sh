89be3f1f51f9ffabd4c45b83b7ea7c3e2e39b35c
mvogelgesang <mvogelgesang@salesforce.com>
2025-06-10 10:01:44 -0400

    #20 initial function creation and migration of title to _message function

--- src/functions.sh
+++ src/functions.sh
@@ -1,4 +1,30 @@
+#!/bin/bash
 
+## Colors
+export oc_COLOR_WHITE='\033[0;37m' #cWhite
+# ='\033[0;34m'  #cBlue
+# ='\033[0;36m' #cCyan
+export oc_COLOR_QUESTION='\033[0;96m' #cLightCyan
+#='\033[0;94m' #cLightBlue
+export oc_COLOR_SUCCESS='\033[0;32m' #cGreen
+export oc_COLOR_WARN='\033[0;33m' #cYellow
+export oc_COLOR_THEME='\033[0;35m' #cMagenta
+export oc_COLOR_ERROR='\033[0;31m' #cRed
+export oc_COLOR_NOTIFICATION='\033[0;44m' #bBlue
+#='\033[0;46m' #bCyan
+#='\033[0;104m' #bLightBlue
+#='\033[0;106m' #bLightCyan
+export oc_COLOR_NOCOLOR='\033[0;0m' #cNoColor
+export oc_arrow="  -> "
+export oc_TITLE="
+===================================================
+ __   __   __      __   __   ___      ___  __   __  
+/  \ |__) / _\`    /  \` |__) |__   /\   |  /  \ |__) 
+\__/ |  \ \__>    \__, |  \ |___ /~~\  |  \__/ |  \\
+=================================================== 
+"
+
+# Function to update config variables
 update_or_add_var() {
   local var_name=$1
   local config_file=$2
@@ -22,4 +48,33 @@ update_or_add_var() {
     # If it doesn't exist, add it
     echo -e "\nexport $var_name=$var_value_string" >> "$config_file"
   fi
+}
+
+# A function to echo out pretty messages
+# usage:
+# _message "type" "message"
+#   type: question, warning, error, header, info
+#   message: string to echo out
+_message() {
+  TYPE=$1
+  MESSAGE=$2
+
+  case $TYPE in
+    "question")
+      echo -e "${oc_COLOR_QUESTION}${MESSAGE}${oc_COLOR_NOCOLOR}"
+      ;;
+    "warning")
+      echo -e "${oc_COLOR_WARN}${MESSAGE}${oc_COLOR_NOCOLOR}" >&2
+      ;;
+    "error")
+      echo -e "${oc_COLOR_ERROR}${MESSAGE}${oc_COLOR_NOCOLOR}" >&2
+      ;;
+    "theme")
+      echo -e "${oc_COLOR_THEME}${MESSAGE}${oc_COLOR_NOCOLOR}" >&2
+      ;;
+    *)
+      # Default case for info, headers, etc.
+      echo "${MESSAGE}"
+      ;;
+  esac
 }
