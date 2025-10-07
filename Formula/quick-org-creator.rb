23df650cdd75fe1de62aa4882b6f32098b44f3e3
mvogelgesang <mvogelgesang@salesforce.com>
2025-10-07 11:04:33 -0400

    Update Homebrew formula for v0.4.3

--- Formula/quick-org-creator.rb
+++ Formula/quick-org-creator.rb
@@ -1,11 +1,11 @@
 class QuickOrgCreator < Formula
   desc "Script to ease the process of creating Salesforce scratch orgs"
   homepage "https://github.com/mvogelgesang/homebrew-quick-org-creator"
-  # LOCAL url "https://github.com/mvogelgesang/quick-org-creator/archive/refs/tags/v0.4.2.tar.gz"
-  # LOCAL sha256 "6c477aeee4666c462f5ef581ac729fc2811b33806385334c92fb156b74a38f34"
-  url "https://github.com/mvogelgesang/quick-org-creator/archive/refs/tags/v0.4.2.tar.gz"
-  version "0.4.2"
-  sha256 "6c477aeee4666c462f5ef581ac729fc2811b33806385334c92fb156b74a38f34"
+  # LOCAL url "https://github.com/mvogelgesang/quick-org-creator/archive/refs/tags/v0.4.3.tar.gz"
+  # LOCAL sha256 "7fa2b16bd0717cc877c430f4001e617557f2b25db6233c8067472a936f421bbf"
+  url "https://github.com/mvogelgesang/quick-org-creator/archive/refs/tags/v0.4.3.tar.gz"
+  version "0.4.3"
+  sha256 "7fa2b16bd0717cc877c430f4001e617557f2b25db6233c8067472a936f421bbf"
   license "MIT"
 
   depends_on "gh"
